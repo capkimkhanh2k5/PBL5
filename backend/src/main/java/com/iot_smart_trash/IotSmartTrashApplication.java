a00b276c9d7bf97ab766fb028049da42e0b3bae8
Cáp Kim Khánh <capkimkhanh2k5@gmail.com>
2026-01-27 14:45:31 +0700

    update: template BackEnd, FrontEnd, Flutter

---
+++ backend/src/main/java/com/iot_smart_trash/IotSmartTrashApplication.java
@@ -0,0 +1,13 @@
+package com.iot_smart_trash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IotSmartTrashApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IotSmartTrashApplication.class, args);
+	}
+
+}
