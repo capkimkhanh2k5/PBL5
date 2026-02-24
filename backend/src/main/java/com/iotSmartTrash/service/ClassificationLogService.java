e98cc7b56fea6fb334097558dfe6dd45bf5f266e
Cáp Kim Khánh <capkimkhanh2k5@gmail.com>
2026-02-24 19:27:55 +0700

    fix: insert catch Exception of Thread in process new Thread handle save & delete image on FireBase

--- backend/src/main/java/com/iotSmartTrash/service/ClassificationLogService.java
+++ backend/src/main/java/com/iotSmartTrash/service/ClassificationLogService.java
@@ -1,11 +1,14 @@
 package com.iotSmartTrash.service;
 
+import com.google.cloud.Timestamp;
 import com.google.cloud.firestore.DocumentReference;
 import com.google.cloud.firestore.Firestore;
 import com.iotSmartTrash.exception.ServiceException;
 import com.iotSmartTrash.model.ClassificationLog;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.concurrent.ExecutionException;
 
 @Service
 @RequiredArgsConstructor
@@ -19,11 +22,13 @@ public class ClassificationLogService {
         try {
             DocumentReference docRef = firestore.collection(COLLECTION_NAME).document();
             log.setLogId(docRef.getId());
-            log.setClassifiedAt(com.google.cloud.Timestamp.now());
+            log.setClassifiedAt(Timestamp.now());
             return docRef.set(log).get().getUpdateTime().toString();
-        } catch (Exception e) {
+        } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
-            throw new ServiceException("Cannot save classification log", e);
+            throw new ServiceException("Cannot save classification log: operation interrupted", e);
+        } catch (ExecutionException e) {
+            throw new ServiceException("Cannot save classification log", e.getCause());
         }
     }
 }
