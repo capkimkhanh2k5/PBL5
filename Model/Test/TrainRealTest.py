1b74a55e5f1407c0d1263036bc446a6e3424f454
AnhThu <102230216@sv1.dut.udn.vn>
2026-05-30 18:37:05 +0700

    RealTestData

--- Model/Test/TrainRealTest.py
+++ Model/Test/TrainRealTest.py
@@ -778,7 +778,7 @@ STATE_COOLDOWN  = "COOLDOWN"
 # 15. CAMERA & BACKGROUND SUBTRACTOR
 # ============================================================
 
-cap = cv2.VideoCapture("http://192.168.116.185:4747/video")
+cap = cv2.VideoCapture("http://192.168.4.54:4747/video")
 if not cap.isOpened():
     raise RuntimeError(f"[FATAL] Không mở được camera ID={CAMERA_ID}")
 
